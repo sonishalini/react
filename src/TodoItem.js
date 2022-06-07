c52d1e1eb9d90505d2df14752ed93d4d7c7b3fa2
sonishalini <shalinisoni25@gmail.com>
2022-06-07 17:53:51 +0530

    Edit & Cancel workingProcess

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,15 +2,49 @@ import React from "react";
 
 class TodoItem extends React.Component
 {
+    state ={
+        isEditing : false
+    };
+
+    toggleEditing =() =>
+    {
+        this.setState
+        ({
+            isEditing: !this.state.isEditing
+        })
+    }
+
     clickHandler =() =>{
         this.props.toggleComplete(this.props.index);
     }
+    
     deleteTodo =() =>{
         this.props.deletetTodoFromState(this.props.index);
     }
+
+    editTodoSubmitHandler = (event) => {
+        event.preventDefault();
+    }
+
     render()
     {
         const {todo} =this.props;
+        
+        if (this.state.isEditing)
+         {
+             return (
+                 <li>
+                     <form onSubmit={this.editTodoSubmitHandler}>
+                         <input type="text" defaultValue={todo.text}/>
+                         <button type="submit">Save</button>
+                         <button onClick={this.toggleEditing}>Cancel</button>
+                     </form>
+                 </li>
+             );
+         }
+
+
+
         return(
             <li 
                 
@@ -18,6 +52,7 @@ class TodoItem extends React.Component
             >
                <sapan  onClick={this.clickHandler}> {todo.text}</sapan>
                 <span>
+                    <button onClick={this.toggleEditing}>Edit</button>
                     <button onClick={this.deleteTodo}>Delete</button>
                 </span>
             </li>
